6bd9eea3f9f9a41608ff351287e0891c0b3a3425
Kyuubi0 <uftsimge@gmail.com>
2026-01-10 06:19:10 +0300

    go

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("mousemove", (e) => {
     const bg = document.getElementById("parallax-bg");
-    const x = (window.innerWidth / 2 - e.pageX) / 25;
-    const y = (window.innerHeight / 2 - e.pageY) / 25;
-    bg.style.transform = `translate(${x}px, ${y}px)`;
+    const moveX = (window.innerWidth / 2 - e.pageX) / 30;
+    const moveY = (window.innerHeight / 2 - e.pageY) / 30;
+    bg.style.transform = `translate(${moveX}px, ${moveY}px)`;
 });
